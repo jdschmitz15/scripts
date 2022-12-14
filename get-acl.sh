7790beb35e8c6b99aa41467041497ca64aba4350
jeff.schmitz <jeff.schmitz@illumio.com>
2022-12-14 06:46:19 -0500

    add comments

--- get-acl.sh
+++ get-acl.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#Script to kick off NEN ACL create, wait for ACL create to Finish, Get ACL data file and then applie
+#Requires API user and Key 
+
  
 #Start ACL build process
 curl -s -u api_16ed2a40d66fb4003:1ab9f0644d0e14d02b972ec8f6f356ef2b4d2589f09d1b7575b76d5350fd4525 -H "Content-Type: application/json"  -d {} -X POST https://jds-demo-snc.poc.segmentationpov.com:8443/api/v2/orgs/1/network_devices/6139c31b-95cc-4cb3-8ff6-2363f44aba95/enforcement_instructions_request > /dev/null 2>&1
